7abb73d0ee5008ece1bf4949112a8ca4b6053a60
JacobLars <jacoblar2001@gmail.com>
2024-03-01 11:18:34 +0100

    feat: add navbar component

---
+++ client/app/components/navbar/NavBar.tsx
@@ -0,0 +1,41 @@
+import { Link } from "@nextui-org/react";
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <div>
+      <nav className="bg-slate-600 text-white flex flex-row items-center justify-around h-10">
+        <Link
+          className="hover:bg-slate-700 h-full w-full flex justify-center"
+          href="/"
+        >
+          <p>Home</p>
+        </Link>
+        <Link
+          className="hover:bg-slate-700 h-full w-full flex justify-center"
+          href="/guitars"
+        >
+          <p>Guitars</p>
+        </Link>
+        <Link
+          className="hover:bg-slate-700 h-full w-full flex justify-center"
+          href="/tuners"
+        >
+          <p>Tuners</p>
+        </Link>
+        <Link
+          className="hover:bg-slate-700 h-full w-full flex justify-center"
+          href="/amplifiers"
+        >
+          <p>Amplifiers</p>
+        </Link>
+        <Link
+          className="hover:bg-slate-700 h-full w-full flex justify-center"
+          href="/other"
+        >
+          <p>Other</p>
+        </Link>
+      </nav>
+    </div>
+  );
+};

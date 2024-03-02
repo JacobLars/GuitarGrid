8d890c2e91f6c3cf550658fde3604e65f7d375f9
JacobLars <jacoblar2001@gmail.com>
2024-03-02 15:48:29 +0100

    refactor: move header to it's own component

---
+++ client/app/components/home/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="flex flex-col items-center m-5">
+      <img className="h-16" src="guitargrid.png"></img>
+      <p className="text-2xl">Guitar Grid</p>
+    </div>
+  );
+};
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,12 @@
 import { Spinner } from "@nextui-org/react";
 import { NavBar } from "./components/navbar/NavBar";
 import { Products } from "./components/home/Products";
+import { Header } from "./components/home/Header";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="flex flex-col items-center m-5">
-        <img className="h-16" src="guitargrid.png"></img>
-        <p className="text-2xl">Guitar Grid</p>
-      </div>
+      <Header />
       <NavBar />
       <Products />
     </div>

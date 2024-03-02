9b8e8572de2731aee39bf9db0d4329fa6d78e9eb
JacobLars <jacoblar2001@gmail.com>
2024-03-02 15:47:12 +0100

    feat: add guitargrid logo and title on home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,6 +5,10 @@ import { Products } from "./components/home/Products";
 export default function Home() {
   return (
     <div className="">
+      <div className="flex flex-col items-center m-5">
+        <img className="h-16" src="guitargrid.png"></img>
+        <p className="text-2xl">Guitar Grid</p>
+      </div>
       <NavBar />
       <Products />
     </div>

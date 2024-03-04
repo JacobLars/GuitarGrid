1e1ca3a3bdf8955bbc9c6141adb15a888c9418b8
JacobLars <jacoblar2001@gmail.com>
2024-03-04 16:58:49 +0100

    feat: add loading gif in product page

--- client/app/components/products/ProductPage.tsx
+++ client/app/components/products/ProductPage.tsx
@@ -12,18 +12,19 @@ type Props = {
 export const ProductPage = ({ productId }: Props) => {
   const [guitar, setGuitar] = useState<Guitar>();
   const [tuner, setTuner] = useState<Tuner>();
-
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const fetchProductById = async () => {
       const response = await fetch(
         `http://localhost:8080/api/v1/products/${productId}`
       );
       const responseToJson = await response.json();
-      console.log(responseToJson);
       if (responseToJson.category === "guitars") {
         setGuitar(responseToJson);
+        setIsLoading(false);
       } else if (responseToJson.category === "tuners") {
         setTuner(responseToJson);
+        setIsLoading(false);
       }
     };
     fetchProductById();
@@ -31,18 +32,24 @@ export const ProductPage = ({ productId }: Props) => {
 
   return (
     <div>
-      {guitar && (
+      {isLoading ? (
+        <img className="h-20 mx-auto my-40" src="/loading.gif" alt="Loading" />
+      ) : (
         <>
-          <ProductHeader
-            productName={guitar.name}
-            productRating={guitar.rating}
-            brandLogo={guitar.brand.logo}
-          />
-          <Carousel images={guitar!.images} />
-          <GuitarInfo guitar={guitar} />
+          {guitar && (
+            <>
+              <ProductHeader
+                productName={guitar.name}
+                productRating={guitar.rating}
+                brandLogo={guitar.brand.logo}
+              />
+              <Carousel images={guitar.images} />
+              <GuitarInfo guitar={guitar} />
+            </>
+          )}
+          {tuner && <Carousel images={tuner.images} />}
         </>
       )}
-      {tuner && <Carousel images={tuner!.images} />}
     </div>
   );
 };

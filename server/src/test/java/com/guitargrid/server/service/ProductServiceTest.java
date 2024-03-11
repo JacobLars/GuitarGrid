1b3d312dc0a96e2571fa76224e72877b03de87b6
JacobLars <jacoblar2001@gmail.com>
2024-03-11 12:20:03 +0100

    feat: unit test for handleRequest method

--- server/src/test/java/com/guitargrid/server/service/ProductServiceTest.java
+++ server/src/test/java/com/guitargrid/server/service/ProductServiceTest.java
@@ -1,12 +1,19 @@
 package com.guitargrid.server.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.guitargrid.server.controller.dto.request.GuitarRequest;
+import com.guitargrid.server.controller.dto.request.ProductRequest;
 import com.guitargrid.server.controller.dto.response.GuitarResponse;
 import com.guitargrid.server.controller.dto.response.ProductListResponse;
+import com.guitargrid.server.controller.dto.response.ProductResponse;
 import com.guitargrid.server.controller.dto.response.TunerResponse;
 import com.guitargrid.server.mapper.ProductMapper;
+import com.guitargrid.server.model.Brand;
 import com.guitargrid.server.model.products.Guitar;
 import com.guitargrid.server.model.products.Tuner;
+import com.guitargrid.server.repository.BrandRepository;
 import com.guitargrid.server.repository.ProductRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,9 +21,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static com.guitargrid.server.utils.BrandTestData.createNewBrandEntity;
 import static com.guitargrid.server.utils.GuitarTestData.*;
 
+import static com.guitargrid.server.utils.ProductTestData.createProductRequest;
+import static com.guitargrid.server.utils.ProductTestData.createProductRequestString;
 import static com.guitargrid.server.utils.TunerTestData.createTunerEntity;
 import static com.guitargrid.server.utils.TunerTestData.createTunerResponse;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,8 +40,12 @@ class ProductServiceTest {
     private ProductRepository productRepository;
 
     @Mock
+    private BrandRepository brandRepository;
+    @Mock
     private ProductMapper productMapper;
 
+    @Mock
+    ObjectMapper objectMapper;
     @InjectMocks
     private ProductService productService;
 
@@ -48,7 +64,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void shouldReturnListContainingTunerWhenQueryingProducts(){
+    void shouldReturnListContainingTunersWhenQueryingProducts(){
         Tuner tuner = createTunerEntity();
         TunerResponse tunerResponse = createTunerResponse();
         when(productRepository.findByCategory("tuners")).thenReturn(List.of(tuner));
@@ -58,6 +74,26 @@ class ProductServiceTest {
         assertEquals(response.tuners().get(0), tunerResponse);
         assertEquals(response.tuners().size(), 1);
         assertEquals(response.tuners().get(0).name(), tuner.getName());
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnProductResponseWhenHandlingRequest(){
+        String electricGuitarRequest = createProductRequestString();
+        ProductRequest productRequest = createProductRequest();
+        Guitar electricGuitar = createElectricGuitarEntity();
+        GuitarResponse electricGuitarResponse = createElectricGuitarResponse();
+        GuitarRequest guitarRequest = createGuitarRequest();
+        Brand brand = createNewBrandEntity();
+        ProductResponse productResponse = ProductResponse.builder().guitar(electricGuitarResponse).build();
+        when(objectMapper.readValue(electricGuitarRequest, ProductRequest.class)).thenReturn(productRequest);
+        when(objectMapper.readValue(electricGuitarRequest, GuitarRequest.class)).thenReturn(guitarRequest);
+        when(productMapper.mapRequestToProduct(guitarRequest)).thenReturn(electricGuitar);
+        when(brandRepository.findById(brand.getId())).thenReturn(Optional.of(brand));
+        when(productRepository.save(electricGuitar)).thenReturn(electricGuitar);
+        when(productMapper.mapToProductResponse(electricGuitar)).thenReturn(productResponse);
+        ProductResponse product = productService.handleRequest(electricGuitarRequest, brand.getId());
+        assertEquals(product.guitar().name(), guitarRequest.getName());
     }
 
 }
--- server/src/test/java/com/guitargrid/server/utils/ProductTestData.java
+++ server/src/test/java/com/guitargrid/server/utils/ProductTestData.java
@@ -1,11 +1,11 @@
 package com.guitargrid.server.utils;
 
+import com.guitargrid.server.controller.dto.request.ProductRequest;
 import com.guitargrid.server.controller.dto.response.GuitarResponse;
 import com.guitargrid.server.controller.dto.response.ProductListResponse;
 import com.guitargrid.server.controller.dto.response.ProductResponse;
 import com.guitargrid.server.controller.dto.response.TunerResponse;
-import com.guitargrid.server.model.products.Guitar;
-import com.guitargrid.server.model.products.Product;
+import com.guitargrid.server.model.Image;
 import lombok.experimental.UtilityClass;
 
 import java.util.List;
@@ -15,6 +15,7 @@ public class ProductTestData {
 
     public static final String BASE_URL_PRODUCTS = "/api/v1/products";
 
+    List<Image> images = List.of(new Image());
     public static ProductListResponse createProductListResponseWithTuners(TunerResponse tunerResponse) {
         return ProductListResponse.builder()
                 .tuners(List.of(tunerResponse))
@@ -31,6 +32,36 @@ public class ProductTestData {
         return ProductListResponse.builder()
                 .guitars(List.of(guitarResponse))
                 .build();
+    }
+
+    public static String createProductRequestString() {
+        return "{\n" +
+                "  \"name\": \"Stratocaster\",\n" +
+                "  \"price\": 100,\n" +
+                "  \"images\": [],\n" +
+                "  \"rating\": 5,\n" +
+                "  \"productQuantity\": 10,\n" +
+                "  \"category\": \"guitars\",\n" +
+                "  \"color\": \"Sunburst\",\n" +
+                "  \"bodyType\": \"Maple\",\n" +
+                "  \"neckType\": \"Maple\",\n" +
+                "  \"fretboardType\": \"Maple\",\n" +
+                "  \"pickups\": \"Single Coil\",\n" +
+                "  \"frets\": 22,\n" +
+                "  \"scale\": 255,\n" +
+                "  \"type\": \"electric\"\n" +
+                "}";
+    }
+
+    public static ProductRequest createProductRequest(){
+        return new ProductRequest(
+                "Stratocaster",
+                100,
+                images,
+                5,
+                10,
+                "guitars"
+        );
     }
 
 }

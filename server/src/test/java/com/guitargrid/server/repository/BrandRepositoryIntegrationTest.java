312456e9fa5e74307356652e4a82c8f38b6224c9
JacobLars <jacoblar2001@gmail.com>
2024-03-12 12:54:42 +0100

    feat: add h2 database & integration test for BrandRepository

---
+++ server/src/test/java/com/guitargrid/server/repository/BrandRepositoryIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.guitargrid.server.repository;
+
+import com.guitargrid.server.model.Brand;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+
+
+@ActiveProfiles("test")
+@DataJpaTest
+public class BrandRepositoryIntegrationTest {
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Test
+    void shouldFindBrandByIdAndReturnBrand() {
+        Brand newBrand = Brand.builder()
+                .name("Fender")
+                .logo("fender.png")
+                .build();
+        Brand savedBrand = brandRepository.save(newBrand);
+        Brand foundBrand = brandRepository.findById(savedBrand.getId()).orElse(null);
+        assertThat(foundBrand).isNotNull();
+        assert foundBrand != null;
+        assertThat(foundBrand.getName()).isEqualTo(newBrand.getName());
+    }
+
+}

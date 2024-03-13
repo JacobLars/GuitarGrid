a274b268afd03c90591dfead17bccf2a70b9fb31
JacobLars <jacoblar2001@gmail.com>
2024-03-13 11:57:32 +0100

    fix: some images being half hidden

--- client/app/components/products/ProductCard.tsx
+++ client/app/components/products/ProductCard.tsx
@@ -10,14 +10,16 @@ export const ProductCard = ({ product }: Props) => {
   return (
     <div>
       <Card className="flex items-center shadow-lg m-5 border border-[#c5c5c5]">
-        <img
-          className="w-40 max-h-40 self-start p-5 object-contain -ml-10 overflow-hidden"
-          src={product.images.filter((img) => img.isMain === true)[0].url}
-        />
-        <div className="ml-10 -mt-36">
+        <div className="w-1/2 max-h-40 self-start jusitfy-center">
+          <img
+            className="w-44 max-h-44 self-start p-5 object-contain"
+            src={product.images.filter((img) => img.isMain === true)[0].url}
+          />
+        </div>
+        <div className="ml-10 -mt-36 self-end w-1/2">
           <div className="flex">
             <p className="mr-2 font-bold">{product.brandName}</p>
-            <p>{product.name}</p>
+            <p className="w-32">{product.name}</p>
           </div>
           <Rating
             className="mt-2"

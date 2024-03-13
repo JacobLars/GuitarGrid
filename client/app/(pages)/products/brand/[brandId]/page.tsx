c59adfcbb2e79c6c54af9a371becc339b1f81adc
JacobLars <jacoblar2001@gmail.com>
2024-03-13 13:57:09 +0100

    feat: page for displaying products for specific brand

---
+++ client/app/(pages)/products/brand/[brandId]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BrandProducts() {
+  return (
+    <div className="">
+      <h2>Brand products page</h2>
+    </div>
+  );
+}
--- client/app/components/brands/BrandCard.tsx
+++ client/app/components/brands/BrandCard.tsx
@@ -1,13 +1,16 @@
 import { Brand } from "@/app/types/Types";
+import Link from "next/link";
 import React from "react";
 type Props = {
   brand: Brand;
 };
 export const BrandCard = ({ brand }: Props) => {
   return (
-    <div className="flex flex-col border border-slate-600 items-center rounded-md p-2">
-      <img className="w-40" src={brand.logo} />
-      <p>{brand.name}</p>
-    </div>
+    <Link href={`/products/brand/${brand.id}`}>
+      <div className="flex flex-col border border-slate-600 items-center justify-center rounded-md p-5 h-28">
+        <img className="w-40" src={brand.logo} />
+        <p>{brand.name}</p>
+      </div>
+    </Link>
   );
 };

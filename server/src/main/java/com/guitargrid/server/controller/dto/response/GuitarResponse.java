3d40272669dab2c6a296bb02ee3eb041c4cbcfd7
JacobLars <jacoblar2001@gmail.com>
2024-03-14 11:35:39 +0100

    fix: ProductService tests

--- server/src/main/java/com/guitargrid/server/controller/dto/response/GuitarResponse.java
+++ server/src/main/java/com/guitargrid/server/controller/dto/response/GuitarResponse.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @Getter
 @Setter
 public class GuitarResponse extends ProductResponse {
+
     private String color;
     private String bodyType;
     private String neckType;
--- server/src/test/java/com/guitargrid/server/service/ProductServiceTest.java
+++ server/src/test/java/com/guitargrid/server/service/ProductServiceTest.java
@@ -1,13 +1,36 @@
 package com.guitargrid.server.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.guitargrid.server.controller.dto.request.GuitarRequest;
+import com.guitargrid.server.controller.dto.request.ProductRequest;
+import com.guitargrid.server.controller.dto.response.GuitarResponse;
+import com.guitargrid.server.controller.dto.response.ProductListResponse;
+import com.guitargrid.server.controller.dto.response.ProductResponse;
+import com.guitargrid.server.controller.dto.response.TunerResponse;
 import com.guitargrid.server.mapper.ProductMapper;
+import com.guitargrid.server.model.Brand;
+import com.guitargrid.server.model.products.Guitar;
+import com.guitargrid.server.model.products.Tuner;
 import com.guitargrid.server.repository.BrandRepository;
 import com.guitargrid.server.repository.ProductRepository;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.guitargrid.server.utils.BrandTestData.createNewBrandEntity;
+import static com.guitargrid.server.utils.GuitarTestData.*;
+import static com.guitargrid.server.utils.ProductTestData.*;
+import static com.guitargrid.server.utils.TunerTestData.createTunerEntity;
+import static com.guitargrid.server.utils.TunerTestData.createTunerResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -28,36 +51,34 @@ class ProductServiceTest {
     @InjectMocks
     private ProductService productService;
 
-/*    @Test
-    void shouldReturnListContainingAcousticGuitarsWhenQueryingProducts() {
+    @Test
+    void shouldReturnListOfQueriedAcousticGuitars() {
        Guitar accousticGuitar = createAcousticGuitarEntity();
        Guitar electricGuitar = createElectricGuitarEntity();
        GuitarResponse accousticGuitarResponse = createAcousticGuitarResponse();
        when(productRepository.findByCategory("guitars")).thenReturn(List.of(accousticGuitar, electricGuitar));
        when(productMapper.mapCategoryToProductListResponse(List.of(accousticGuitar)))
-               .thenReturn(ProductListResponse.builder().guitars(List.of(accousticGuitarResponse)).build());
+               .thenReturn(ProductListResponse.builder().products(List.of(accousticGuitarResponse)).build());
        ProductListResponse response = productService.getQueriedProducts("guitars", "acoustic");
-       assertEquals(response.guitars().get(0), accousticGuitarResponse);
-       assertEquals(response.guitars().size(), 1);
-       assertEquals(response.guitars().get(0).name(), accousticGuitar.getName());
-         assertInstanceOf(GuitarResponse.class, response.guitars().get(0));
+       assertEquals(response.products().get(0), accousticGuitarResponse);
+       assertEquals(response.products().size(), 1);
+       assertEquals(response.products().get(0).getName(), accousticGuitar.getName());
     }
 
     @Test
-    void shouldReturnListContainingTunersWhenQueryingProducts(){
+    void shouldReturnListOfQueriedTuners(){
         Tuner tuner = createTunerEntity();
         TunerResponse tunerResponse = createTunerResponse();
         when(productRepository.findByCategory("tuners")).thenReturn(List.of(tuner));
         when(productMapper.mapCategoryToProductListResponse(List.of(tuner)))
-                .thenReturn(ProductListResponse.builder().tuners(List.of(tunerResponse)).build());
+                .thenReturn(ProductListResponse.builder().products(List.of(tunerResponse)).build());
         ProductListResponse response = productService.getQueriedProducts("tuners", null);
-        assertEquals(response.tuners().get(0), tunerResponse);
-        assertEquals(response.tuners().size(), 1);
-        assertEquals(response.tuners().get(0).name(), tuner.getName());
-        assertInstanceOf(TunerResponse.class, response.tuners().get(0));
-    }*/
+        assertEquals(response.products().get(0), tunerResponse);
+        assertEquals(response.products().size(), 1);
+        assertEquals(response.products().get(0).getName(), tuner.getName());
+        assertInstanceOf(TunerResponse.class, response.products().get(0));
+    }
 
-/*
     @Test
     @SneakyThrows
     void shouldReturnGuitarResponseWhenHandlingGuitarRequest(){
@@ -67,7 +88,7 @@ class ProductServiceTest {
         GuitarResponse electricGuitarResponse = createElectricGuitarResponse();
         GuitarRequest guitarRequest = createGuitarRequest();
         Brand brand = createNewBrandEntity();
-        ProductResponse productResponse = ProductResponse.builder().guitar(electricGuitarResponse).build();
+        ProductResponse productResponse = createProductResponse(electricGuitarResponse);
         when(objectMapper.readValue(electricGuitarRequest, ProductRequest.class)).thenReturn(productRequest);
         when(objectMapper.readValue(electricGuitarRequest, GuitarRequest.class)).thenReturn(guitarRequest);
         when(productMapper.mapRequestToProduct(guitarRequest)).thenReturn(electricGuitar);
@@ -75,9 +96,9 @@ class ProductServiceTest {
         when(productRepository.save(electricGuitar)).thenReturn(electricGuitar);
         when(productMapper.mapToProductResponse(electricGuitar)).thenReturn(productResponse);
         ProductResponse product = productService.handleRequest(electricGuitarRequest, brand.getId());
-        assertEquals(product.guitar().name(), guitarRequest.getName());
-        assertInstanceOf(GuitarResponse.class, product.guitar());
+        assertEquals(product.getName(), guitarRequest.getName());
+        assertEquals(product.getPrice(), guitarRequest.getPrice());
+        assertEquals(product.getRating(), guitarRequest.getRating());
     }
-*/
 
 }

5f3b1f594064500c8f28770a9af022ec0f9fc79d
JacobLars <jacoblar2001@gmail.com>
2024-03-20 15:09:26 +0100

    feat: only display hamburger menu on mobile screen

--- client/app/components/filtering/FilterMenu.tsx
+++ client/app/components/filtering/FilterMenu.tsx
@@ -27,27 +27,24 @@ export const FilterMenu = ({
   return (
     <div className="mt-5 mb-16">
       <img
-        className="h-8 absolute mt-2 ml-14 z-10 hover:cursor-pointer hover:bg-slate-100 md:left-1/4"
+        className="h-8 absolute mt-2 ml-14 z-10 hover:cursor-pointer hover:bg-slate-100 md:left-1/4 md:hidden"
         src={icon}
         onClick={handleToggle}
       />
       <div
-        className={`mx-auto w-3/4 transition-transform ease-out duration-300 md:w-1/3 ${
+        className={`mx-auto w-3/4 transition-transform ease-out duration-300 md:w-1/3 md:border md:border-slate-600 md:rounded-md md:shadow-md ${
           isOpen
-            ? "translate-x-0  border border-slate-600 rounded-md shadow-md"
+            ? "translate-x-0 border border-slate-600 rounded-md shadow-md"
             : "translate-x-full"
         }`}
       >
-        {isOpen && (
-          <>
-            <FilterByRating
-              products={products}
-              setSelectedRatings={setSelectedRatings}
-              selectedRatings={selectedRatings}
-            />
-            <SortByPrice setOption={setOption} />
-          </>
-        )}
+        {/* Always render FilterByRating and SortByPrice */}
+        <FilterByRating
+          products={products}
+          setSelectedRatings={setSelectedRatings}
+          selectedRatings={selectedRatings}
+        />
+        <SortByPrice setOption={setOption} />
       </div>
     </div>
   );

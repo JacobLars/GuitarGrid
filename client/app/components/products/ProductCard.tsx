ba26ade0db2ba44c7376c6ad6cd2aa193d6567d8
JacobLars <jacoblar2001@gmail.com>
2024-03-21 17:00:07 +0100

    feat: add add to cart button for desktop

--- client/app/components/products/ProductCard.tsx
+++ client/app/components/products/ProductCard.tsx
@@ -9,17 +9,17 @@ type Props = {
 export const ProductCard = ({ product }: Props) => {
   return (
     <div>
-      <Card className="flex items-center shadow-lg m-5 border border-[#c5c5c5]">
+      <Card className="flex items-center shadow-lg m-5 border border-[#c5c5c5] md:h-44">
         <div className="w-1/2 h-44 self-start jusitfy-center">
           <img
             className="w-44 h-44 self-start p-5 object-contain"
             src={product.images.filter((img) => img.isMain === true)[0].url}
           />
         </div>
-        <div className="ml-10 -mt-36 self-end w-1/2">
+        <div className="ml-10 -mt-36 self-end w-1/2 md:self-start md:ml-40">
           <div className="flex">
             <p className="mr-2 font-bold">{product.brandName}</p>
-            <p className="w-32">{product.name}</p>
+            <p className="w-auto">{product.name}</p>
           </div>
           <Rating
             className="mt-2"
@@ -27,8 +27,14 @@ export const ProductCard = ({ product }: Props) => {
             value={product.rating}
             readOnly
           />
-          <p className="mt-2 text-lg">{product.price}$</p>
+          <p className="mt-2 text-lg md:hidden">{product.price}$</p>
           <InStock product_quantity={product.product_quantity} />
+        </div>
+        <div className="hidden md:flex md:flex-col md:items-center md:self-end mr-5">
+          <p className="font-bold -mt-24 text-2xl">{product.price}$</p>
+          <button className="mt-14 bg-slate-600 text-white p-2 rounded-md hover:bg-slate-800 z-10">
+            Add to Cart
+          </button>
         </div>
       </Card>
     </div>

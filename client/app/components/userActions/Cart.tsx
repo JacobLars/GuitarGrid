37e098d68e9e123bb7e0fa5bc4cb46c7b589e558
JacobLars <jacoblar2001@gmail.com>
2024-04-01 16:59:01 +0200

    feat: increment cart quantity when clicking Add To Cart button on product page

--- client/app/components/userActions/Cart.tsx
+++ client/app/components/userActions/Cart.tsx
@@ -4,12 +4,11 @@ type Props = {
   cartProducts: Product[];
 };
 export const Cart = ({ cartProducts }: Props) => {
-  const [count, setCount] = useState<number>(0);
   return (
     <div>
       <img className="h-8" src="/cart.png" />
       <p className="rounded-full text-center w-5 h-5 bg-red-300 -mt-10 ml-5 absolute">
-        {count}
+        {cartProducts ? cartProducts.length : 0}
       </p>
     </div>
   );

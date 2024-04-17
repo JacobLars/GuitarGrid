7a76e6e086f0e64392286f5c00d60aac854f5dfa
JacobLars <jacoblar2001@gmail.com>
2024-04-17 22:43:43 +0200

    feat: display cart items from localstorage

--- client/app/components/cart/CartItems.tsx
+++ client/app/components/cart/CartItems.tsx
@@ -1,17 +1,21 @@
-"use client";
 import { Product } from "@/app/types/Types";
 import React, { useEffect } from "react";
 
 export const CartItems = () => {
-  let cart: Product[];
+  let cart: { product: Product; quantity: number }[] = [];
 
-  useEffect(() => {
-    cart = JSON.parse(localStorage.getItem("cart") as string);
-  }, []);
+  cart = JSON.parse(localStorage.getItem("cart") as string);
 
+  console.log(cart);
   return (
     <div>
-      <ul className="border border-t-sky-400"></ul>
+      <ul className="border border-t-sky-400">
+        {cart?.map((item, index) => (
+          <li key={index}>
+            <h1>{item.product.name}</h1>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         <Categories />
         <BrandGallery />
       </div>
+      <CartItems />
     </div>
   );
 }

fe717b93e8653b9d78eb442d653892bcb935fe5e
JacobLars <jacoblar2001@gmail.com>
2024-04-18 19:35:57 +0200

    feat: styling for cart item list

--- client/app/components/cart/CartItems.tsx
+++ client/app/components/cart/CartItems.tsx
@@ -4,14 +4,30 @@ import React, { useEffect } from "react";
 export const CartItems = () => {
   let cart: { product: Product; quantity: number }[] = [];
 
-  cart = JSON.parse(localStorage.getItem("cart") as string);
-
+  const getCart = async () => {
+    cart = JSON.parse(localStorage.getItem("cart") as string);
+  };
+  getCart();
   return (
     <div>
-      <ul className="border border-t-sky-400 z-10">
+      <ul className="border border-slate-600 rounded-md z-10 p-2">
         {cart?.map((item, index) => (
-          <li key={index}>
-            <h1>{item.product.name}</h1>
+          <li className="border-b border-b-slate-600" key={index}>
+            <div className="flex flex-row items-center">
+              <div className="p-2 -ml-2">
+                <img
+                  className="h-16 w-16"
+                  src={
+                    item.product.images.filter((img) => img.isMain === true)[0]
+                      .url
+                  }
+                />
+              </div>
+              <div className="ml-2">
+                <p>{item.product.name}</p>
+                <p className="text-xl">{item.product.price}$</p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
--- client/app/components/userActions/Cart.tsx
+++ client/app/components/userActions/Cart.tsx
@@ -15,7 +15,11 @@ export const Cart = () => {
   };
   return (
     <div>
-      <img onClick={handleCartOnClick} className="h-8" src="/cart.png" />
+      <img
+        onClick={handleCartOnClick}
+        className="h-8 hover:cursor-pointer"
+        src="/cart.png"
+      />
       <p className="rounded-full text-center w-5 h-5 bg-red-300 -mt-10 ml-5 absolute">
         {cartItemCount}
       </p>
